e14af15e6c55d5e53aea1fbd73617037aea06568
jamunashri <jamunashrishanmugasundaram@gmail.com>
2022-08-21 11:40:31 +0530

    description changed

--- src/AboutMe/index.jsx
+++ src/AboutMe/index.jsx
@@ -7,11 +7,14 @@ const AboutMe = () => {
 				<h1>About Me </h1>
 
 				<p>
-                Front End developer with industry experience building websites and web applications. I specialize in JavaScript and have professional experience working with React and Node JS. I also have experience working with MongoDB collections, and MySQL. I specialize in Agile development process.
-				</p>
-				<p>
+					Frontend Developer who loves Challenges. I have worked as a Web developer for the past 2 years, delivering great results to my employers and automatic processes which helped the companies to be more efficient. My technical background in Engineering helps me always find the best solution to a problem. This combined with my passion for learning and being challenged makes me a great asset to your learning.	</p>
+
+				<p>	As a part of a small team in my last job, I was involved in the entire development cycle of their website. Where we used many technologies like HTML5, CSS3, JavaScript, React JS in the Frontend, and Node JS in the backend. It really thrilled me to see that it is the particualr expertise you are looking for to complete your team. </p>
+
+				<p>	I am constantly and actively seeking out new technologies and staying up-to-date on industry trends because as we know this field moves extremely fast. This has allowed me to stay ahead of the curve and deliver exceptional work.	</p>		
+				{/* <p>
 					Currently I am a working as a Software Engineer in Telaverge communication pvt Ltd, Banglore.
-				</p>
+				</p> */}
 			</div>
 		</div>
 	);
